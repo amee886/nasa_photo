421983cf31914915a1f3960a35bea5dacbef65c5
amee886 <nsecex@gmail.com>
2025-07-06 18:52:35 +0300

    Publish photo to Telegram channel

--- tg_bot.py
+++ tg_bot.py
@@ -2,3 +2,6 @@ import telegram
 bot = telegram.Bot(token='7904978015:AAF5K8b508yayzalS4P6CI0ilavz77eSYAY')
 chat_id="@SpaceXandSpace"
 bot.send_message(text="Hi",chat_id=chat_id)
+bot.send_photo(chat_id=chat_id,
+               photo=open(r"E:\python\kurs\images\nasa_images\nasa_apod_0.jpg","rb")
+               )

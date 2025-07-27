25e502d947e21d355f79a794ba3e5fc7ee785dfd
amee886 <nsecex@gmail.com>
2025-07-27 21:09:04 +0300

    Убрал ненужное и исправил ошибки

--- APOD.py
+++ APOD.py
@@ -2,30 +2,30 @@ import requests
 import os
 from decouple import config
 from urllib.parse import urlsplit
+from download import download_astronomy_photo
 
 
-def APOD(nasa_api_key):
-        url = f'https://api.nasa.gov/planetary/apod?api_key={nasa_api_key}&count=30'
+def astronomy_picture_of_the_day(nasa_api_key, count):
+        params = {
+                "api_key": nasa_api_key,
+                "count": count
+        }
+        url = f'https://api.nasa.gov/planetary/apod'
         nasa_apod = "https://apod.nasa.gov/apod/image/2506/IC2177SeagullLRGB-APOD2048.jpg"
-        response = requests.get(url)
-        data = response.json()
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        json_information = response.json()
         os.makedirs('nasa_images', exist_ok=True)
-        for idx, item in enumerate(data):
-            image_url = item.get('url')
+        for index_photo, astronomy_photo in enumerate(json_information):
+            image_url = astronomy_photo.get('url')
             if image_url and (image_url.endswith(".jpg") or image_url.endswith(".png")):
-                image_data = requests.get(image_url).content
-                image_name = os.path.join('nasa_images', f"nasa_apod_{idx}.jpg")
-                with open(image_name, 'wb') as file:
-                    file.write(image_data)
-        nasa_apod_split = urlsplit(nasa_apod)
-        nasa_apod_path = nasa_apod_split.path
-        nasa_apod_split = os.path.splitext(nasa_apod_path)
-        print(nasa_apod_split[1])
+                download_astronomy_photo(image_url, index_photo)
 
-        
+
 def main():
+        count = 30
         nasa_api_key = config("NASA_API_KEY")
-        APOD(nasa_api_key)
+        astronomy_picture_of_the_day(nasa_api_key, count)
 
         
 if __name__ == '__main__':

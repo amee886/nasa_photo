3bb8ea1117db537ac503be84ecbd1f713d8282e9
amee886 <nsecex@gmail.com>
2025-07-31 19:45:10 +0300

    Update EPIC.py

--- EPIC.py
+++ EPIC.py
@@ -1,33 +1,39 @@
-import requests
 import os
 from decouple import config
-from urllib.parse import urlsplit
-from download import download_astronomy_photo
+from datetime import datetime
+from download import download_earth_photo
 
 
-def astronomy_picture_of_the_day(nasa_api_key, count):
-        params = {
-                "api_key": nasa_api_key,
-                "count": count
-        }
-        url = f'https://api.nasa.gov/planetary/apod'
-        nasa_apod = "https://apod.nasa.gov/apod/image/2506/IC2177SeagullLRGB-APOD2048.jpg"
-        response = requests.get(url, params=params)
+def earth_polychromatic_imaging_camera(limit, nasa_api_key):
+        params={
+                "api_key": nasa_api_key
+                }
+        url_epic = f"https://api.nasa.gov/EPIC/api/natural/images"
+        response = requests.get(url_epic,params=params)
         response.raise_for_status()
         json_information = response.json()
-        os.makedirs('nasa_images', exist_ok=True)
-        for index_photo, astronomy_photo in enumerate(json_information):
-            image_url = astronomy_photo.get('url')
-            if image_url and (image_url.endswith(".jpg") or image_url.endswith(".png")):
-                download_astronomy_photo(image_url, index_photo)
+        os.makedirs("EPIC_images", exist_ok=True)
+        for index_photo, earth_photo in enumerate(json_information):
+                if index_photo >= limit:
+                        break
+
+                image_name = earth_photo.get("image")
+                get_date = earth_photo.get("date")
+                date_imaging=datetime.strptime(get_date,"%Y-%m-%d %H:%M:%S")
+                year_photo=date_imaging.year
+                month_photo=date_imaging.month
+                day_photo=date_imaging.day
+                params={
+                        "api_key": nasa_api_key
+                }
+                created_url = f"https://api.nasa.gov/EPIC/archive/natural/{year_photo:04d}/{month_photo:02d}/{day_photo:02d}/png/{image_name}.png"
+                download_earth_photo(created_url, index_photo,params)
 
 
 def main():
-        count = 30
-        nasa_api_key = config("NASA_API_KEY")
-        astronomy_picture_of_the_day(nasa_api_key, count)
+    nasa_api_key = config("NASA_API_KEY")
+    earth_polychromatic_imaging_camera(10, nasa_api_key)
 
-        
+
 if __name__ == '__main__':
     main()
-

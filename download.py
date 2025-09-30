3ca93c365c67cc228c9f56f1eeabd29771f47258
amee886 <nsecex@gmail.com>
2025-09-30 21:55:06 +0300

    Update download.py

--- download.py
+++ download.py
@@ -2,28 +2,28 @@ import requests
 import os
 
 
-def download_earth_photo(created_url, index_photo,params):
-    image_content = requests.get(created_url,params=params)
-    image_content.raise_for_status()
-    image_file_name = os.path.join('EPIC_images', f"nasa_epic_{index_photo}.png")
-    with open(image_file_name, 'wb') as file:
-        file.write(image_content.content)
+def download_photo(image_url, photo_index, params=None, prefix="image", save_dir="downloaded_images", ext="jpg"):
+    os.makedirs(save_dir, exist_ok=True)
 
+    response = requests.get(image_url, params=params)
+    response.raise_for_status()
 
-def download_astronomy_photo(image_url, index_photo):
-    image_content = requests.get(image_url)
-    image_content.raise_for_status()
-    image_name = os.path.join('nasa_images', f"nasa_apod_{index_photo}.jpg")
-    with open(image_name, 'wb') as file:
-        file.write(image_content.content)
+    filename = f"{prefix}_{photo_index}.{ext}"
+    file_path = os.path.join(save_dir, filename)
 
+    counter = 1
+    while os.path.exists(file_path):
+        new_filename = f"{prefix}_{photo_index}_{counter}.{ext}"
+        file_path = os.path.join(save_dir, new_filename)
+        counter += 1
 
-def download_launch_photo(index_photo, launch_photo):
-    launch_photo_response=requests.get(launch_photo)
-    launch_photo_response.raise_for_status()
-    with open(f"images_{index_photo}.jpg", "wb") as file:
-        file.write(launch_photo_response.content)
+    with open(file_path, "wb") as file:
+        file.write(response.content)
+
+    print(f"Скачано: {file_path}")
+    return file_path
 
 
 if __name__ == '__main__':
     pass
+

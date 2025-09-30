b33b53fb86124fd94b36b721167480ce07404334
amee886 <nsecex@gmail.com>
2025-09-30 21:57:28 +0300

    Update fetch_spacex_last_launch.py

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -1,27 +1,31 @@
 import requests
 import argparse
-from download import download_launch_photo
+from download_gpt import download_photo
 
 
-def fetch_spacex_last_launch(launch_id):
+def download_launch_photo(launch_id):
+        params = {
+                "launch_id": launch_id
+                }
         url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
         response = requests.get(url)
         response.raise_for_status()
         response = response.json()
         images = response.get('links', {}).get('flickr', {}).get('original', [])
 
-        for index_photo, launch_photo in enumerate(images):
-                download_launch_photo(index_photo, launch_photo)
+        for photo_index, image_url in enumerate(images):
+                download_photo(image_url, photo_index, params)
 
-                
+
 def main():
         parser = argparse.ArgumentParser(description='Скачивает фотографии запуска ракеты SpaceX  по id запуска')
         parser.add_argument('--id', type=str, default='5eb87d42ffd86e000604b384', help='id запуска SpaceX(по умолчанию:5eb87d42ffd86e000604b384)')
         args = parser.parse_args()
         launch_id = args.id
-        fetch_spacex_last_launch(launch_id)
+        download_launch_photo(launch_id)
 
-        
+
 if __name__ == '__main__':
     main()
+
 

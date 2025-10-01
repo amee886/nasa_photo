5f4b0d8314d9cdf04b7019bd49e1aed674f91fd8
amee886 <nsecex@gmail.com>
2025-10-01 21:47:17 +0300

    изменил название функции и починил код

--- APOD.py
+++ APOD.py
@@ -2,31 +2,30 @@ import requests
 import os
 from decouple import config
 from urllib.parse import urlsplit
-from download import download_astronomy_photo
+from download_gpt import download_photo
 
 
-def astronomy_picture_of_the_day(nasa_api_key, count):
+def download_astronomy_photo(nasa_api_key, count):
         params = {
                 "api_key": nasa_api_key,
                 "count": count
         }
         url = f'https://api.nasa.gov/planetary/apod'
+        nasa_apod = "https://apod.nasa.gov/apod/image/2506/IC2177SeagullLRGB-APOD2048.jpg"
         response = requests.get(url, params=params)
         response.raise_for_status()
-        json_information = response.json()
-        os.makedirs('nasa_images', exist_ok=True)
-        for index_photo, astronomy_photo in enumerate(json_information):
+        gets_dictionary = response.json()
+        for photo_index, astronomy_photo in enumerate(gets_dictionary):
             image_url = astronomy_photo.get('url')
             if image_url and (image_url.endswith(".jpg") or image_url.endswith(".png")):
-                download_astronomy_photo(image_url, index_photo)
+                download_photo(image_url, photo_index,params)
 
 
 def main():
         count = 30
         nasa_api_key = config("NASA_API_KEY")
-        astronomy_picture_of_the_day(nasa_api_key, count)
+        download_astronomy_photo(nasa_api_key, count)
 
         
 if __name__ == '__main__':
     main()
-

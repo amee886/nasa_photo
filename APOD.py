0714a960e748dee393cab039a8b862994474caf7
amee886 <nsecex@gmail.com>
2025-10-08 22:43:24 +0300

    изменил название переменной

--- APOD.py
+++ APOD.py
@@ -13,7 +13,7 @@ def download_astronomy_photo(nasa_api_key, count):
         url = f'https://api.nasa.gov/planetary/apod'
         response = requests.get(url, params=params)
         response.raise_for_status()
-        gets_dictionary = response.json()
+        decoded_response = response.json()
         for photo_index, astronomy_photo in enumerate(gets_dictionary):
             image_url = astronomy_photo.get('url')
             if image_url and (image_url.endswith(".jpg") or image_url.endswith(".png")):
@@ -28,4 +28,5 @@ def main():
         
 if __name__ == '__main__':
     main()
+
 
